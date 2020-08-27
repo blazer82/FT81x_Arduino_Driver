10631934a243abc4ba44ab01ba6a7992648454a2
Raphael Stäbler <raphael@productionbuild.de>
2020-08-27 15:25:18 +0200

    Add output to doxygen script

--- ci/doxygen.sh
+++ ci/doxygen.sh
@@ -3,9 +3,17 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
 cd $GITHUB_WORKSPACE
 
 # Install doxygen
+echo -e "\n########################################################################";
+echo -e "${YELLOW}INSTALL DOXYGEN"
+echo "########################################################################";
 wget -q http://doxygen.nl/files/doxygen-1.8.18.linux.bin.tar.gz
 tar -xf doxygen-1.8.18.linux.bin.tar.gz
 mv doxygen-1.8.18/bin/doxygen .
@@ -16,6 +24,9 @@ mkdir doxygen_workdir
 cd doxygen_workdir
 
 # Get the current gh-pages branch
+echo -e "\n########################################################################";
+echo -e "${YELLOW}CLONE REPO"
+echo "########################################################################";
 git clone -b gh-pages https://github.com/${GITHUB_REPOSITORY}.git
 export REPO_NAME=${GITHUB_REPOSITORY#*/}
 cd ${REPO_NAME}
@@ -28,9 +39,15 @@ git config user.name "Doxygen CI"
 git config user.email "doxygen@github.com"
 
 # Delete current docs
+echo -e "\n########################################################################";
+echo -e "${YELLOW}DELETE CURRENT DOCS"
+echo "########################################################################";
 rm -rf api || true
 
 # Overwrite output directory
+echo -e "\n########################################################################";
+echo -e "${YELLOW}GENERATE DOCS"
+echo "########################################################################";
 cp Doxyfile ${GITHUB_WORKSPACE}/Doxyfile
 sed -i "s;^HTML_OUTPUT .*;HTML_OUTPUT = doxygen_workdir/${REPO_NAME}/api;" ${GITHUB_WORKSPACE}/Doxyfile
 cd $GITHUB_WORKSPACE
@@ -40,7 +57,7 @@ $GITHUB_WORKSPACE/doxygen Doxyfile
 
 #Check if master branch
 if [ "${GITHUB_REF}" != "refs/heads/master" ]; then
-    echo "Not on master branch, not going to commit to gh-pages!"
+    echo -e "${GREEN}Not on master branch, not going to commit to gh-pages!"
     exit 0
 fi
 
@@ -51,9 +68,11 @@ if [ -d "api" ] && [ -f "api/index.html" ]; then
     git add --all
 
     if [ -n "$(git status --porcelain)" ]; then
-        echo "Changes to commit"
+        echo -e "\n########################################################################";
+        echo -e "${YELLOW}COMMIT CHANGES"
+        echo "########################################################################";
     else
-        echo "No changes to commit"
+        echo -e "${GREEN}No changes to commit"
         exit 0
     fi
 
@@ -61,6 +80,6 @@ if [ -d "api" ] && [ -f "api/index.html" ]; then
 
     git push --force "https://${GITHUB_ACTOR}:${GH_REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" > /dev/null 2>&1
 else
-    echo 'Error: No documentation (html) files have been found!' >&2
+    echo -e "${RED}Error: No documentation files have been found!" >&2
     exit 1
 fi
